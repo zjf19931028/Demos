5d8f64ee8e54c53b9029f9db410727ae065e341d
zhangjingfang <zhangjingfang@jeejio.com>
2021-05-12 23:52:01 +0800

    Feat:change location chatpanel

--- uidemo/src/main/java/com/awesome/uidemo/adapter/AdapterCallback.java
+++
@@ -1,9 +0,0 @@
-package com.awesome.uidemo.adapter;
-
-/**
- * Created by Alice on 2021/4/24
- */
-public interface AdapterCallback<Data> {
-    void update(Data data, RecyclerAdapter.ViewHolder<Data> holder);
-
-}
