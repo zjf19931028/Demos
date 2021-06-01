ee8d1ee63747f381a02e885ab1880f5dfd0aa112
zhangjingfang <zhangjingfang@jeejio.com>
2021-06-01 23:25:53 +0800

    Feat:handlerdemo

---
+++ handlerdemo/src/main/java/com/awesome/handlerdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.awesome.handlerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
